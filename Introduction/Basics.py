dc227fed2d95ea69c40c9810eb38ae0b800bead3
CodesGang <85042651+CodesGang@users.noreply.github.com>
2021-05-30 08:30:25 +0530

    Create Basics.py

---
+++ Introduction/Basics.py
@@ -0,0 +1,61 @@
+'''Reserve Words
+False  Class  return is finally
+None   if     for    lambda continue
+True   def  from  while  nonlocal
+and  del  global  not  with 
+as  elif  try  or  yield
+assert  else  import  pass
+break  except  in  raise'''
+
+print('Hello World')
+
+#Constants
+print(123)
+print('hello world')
+
+#Variable
+x=2
+print(x)
+x=x+2
+print(x)
+x+=2
+print(x)
+
+#eg.
+abcd=35.0
+abed=12.50
+abce=abcd*abed
+print(abce)
+
+#Bodmas
+#Backets Of Division Multiplication Addition Subtraction
+'''
++ Addition
+- Subtraction
+* Multiplication
+/ Division
+** Power
+% Remainder'''
+
+y1=0.3
+y=3.7*y*(1-y)
+z=2+(3*4)-5/(6**7)
+print(z)
+
+#Type
+type(yyy)
+type(12)
+type(12.5)
+
+#Input
+t_n=input("enter:")
+print(t_n)
+
+
+# Conditional statements
+x=5
+if x<5:
+  print('smaller')
+if x<20:
+  print('bigger')
+print("finish")
