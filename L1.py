326e44528ec51150ce9b5518de9c98626190bc0f
CodesGang <85042651+CodesGang@users.noreply.github.com>
2021-05-30 06:21:13 +0530

    Create L1.py

---
+++ L1.py
@@ -0,0 +1,7 @@
+False  Class  return is finally
+None   if     for    lambda continue
+True   def  from  while  nonlocal
+and  del  global  not  with 
+as  elif  try  or  yield
+assert  else  import  pass
+break  except  in  raise
